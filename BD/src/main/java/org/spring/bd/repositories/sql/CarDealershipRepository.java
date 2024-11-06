c3c76480ee7592f40663951d04c7c27d6fedd37f
Ivan Bagay <voewoda88@gmail.com>
2024-11-06 12:54:36 +0300

    feat: init car dealership repo

---
+++ BD/src/main/java/org/spring/bd/repositories/sql/CarDealershipRepository.java
@@ -0,0 +1,7 @@
+package org.spring.bd.repositories.sql;
+
+import org.spring.bd.entities.sql.CarDealership;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarDealershipRepository extends JpaRepository<CarDealership, Integer> {
+}
