087c7516be48913b2f9f8be8e0a0906756805634
Ivan Bagay <voewoda88@gmail.com>
2024-11-06 12:58:08 +0300

    feat: init automobile controller for sql requests

---
+++ BD/src/main/java/org/spring/bd/controllers/api/postgres/AutomobileApiController.java
@@ -0,0 +1,20 @@
+package org.spring.bd.controllers.api.postgres;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.spring.bd.controllers.abstracts.AbstractAutomobileControllers;
+import org.spring.bd.core.dto.AutomobileDTO;
+import org.spring.bd.services.ServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/postgres/api/v1/automobiles")
+@Tag(name = "AutomobileController", description = "Provides CRUD-operations with records in \"automobile\" table")
+public class AutomobileApiController extends AbstractAutomobileControllers {
+    @Autowired
+    public AutomobileApiController(@Qualifier("automobileService") ServiceInterface<AutomobileDTO, Integer> automobileService) {
+        super(automobileService);
+    }
+}
