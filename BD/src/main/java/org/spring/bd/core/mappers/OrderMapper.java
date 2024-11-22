791db85515e4331cda8f0c5fe302ba6ed3f569b8
Ivan Bagay <voewoda88@gmail.com>
2024-11-23 00:37:25 +0300

    update: added methods for mongo objects and convert postgres to mongo

--- BD/src/main/java/org/spring/bd/core/mappers/OrderMapper.java
+++ BD/src/main/java/org/spring/bd/core/mappers/OrderMapper.java
@@ -4,6 +4,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 import org.spring.bd.core.dto.OrderDTO;
+import org.spring.bd.entities.nosql.MongoOrder;
 import org.spring.bd.entities.sql.Order;
 
 @Mapper(componentModel = "spring")
@@ -21,4 +22,18 @@ public interface OrderMapper {
     @Mapping(target = "employer.id", source = "employerId")
     @Mapping(target = "buyer.id", source = "buyerId")
     Order toEntity(OrderDTO orderDTO);
+
+    @Mapping(target = "id", source = "modelId")
+    OrderDTO toDTO(MongoOrder mongoOrder);
+
+    @Mapping(target = "modelId", source = "id")
+    @Mapping(target = "id", ignore = true)
+    MongoOrder toMongoEntity(OrderDTO orderDTO);
+
+    @Mapping(target = "automobileId", source = "automobile.id")
+    @Mapping(target = "carDealershipId", source = "carDealership.id")
+    @Mapping(target = "employerId", source = "employer.id")
+    @Mapping(target = "buyerId", source = "buyer.id")
+    @Mapping(target = "id", ignore = true)
+    MongoOrder entityToMongo(Order order);
 }
