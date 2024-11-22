d8edb253d5eefcbdbe12042365bd18160ae4576e
Ivan Bagay <voewoda88@gmail.com>
2024-11-23 00:36:22 +0300

    update: added custom methods

--- BD/src/main/java/org/spring/bd/repositories/nosql/MongoCarDealershipRepository.java
+++ BD/src/main/java/org/spring/bd/repositories/nosql/MongoCarDealershipRepository.java
@@ -3,6 +3,15 @@ package org.spring.bd.repositories.nosql;
 import org.bson.types.ObjectId;
 import org.spring.bd.entities.nosql.MongoCarDealership;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface MongoCarDealershipRepository extends MongoRepository<MongoCarDealership, ObjectId> {
+    Optional<MongoCarDealership> findByModelId(Integer id);
+
+    boolean existsByModelId(Integer id);
+
+    void deleteByModelId(Integer id);
 }
