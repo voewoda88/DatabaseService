a575860bc36d0d5de77261191308911cf3c5e950
Ivan Bagay <voewoda88@gmail.com>
2024-11-23 00:35:28 +0300

    update: added methods to mongo objects and convert postgres to mongo

--- BD/src/main/java/org/spring/bd/core/mappers/BuyerMapper.java
+++ BD/src/main/java/org/spring/bd/core/mappers/BuyerMapper.java
@@ -1,12 +1,28 @@
 package org.spring.bd.core.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 import org.spring.bd.core.dto.BuyerDTO;
+import org.spring.bd.entities.nosql.MongoBuyer;
 import org.spring.bd.entities.sql.Buyer;
 
 @Mapper(componentModel = "spring")
 public interface BuyerMapper {
+
+    BuyerMapper INSTANCE = Mappers.getMapper(BuyerMapper.class);
+
     BuyerDTO toDTO(Buyer buyer);
 
+    @Mapping(target = "id", source = "id")
     Buyer toEntity(BuyerDTO buyerDTO);
+    @Mapping(target = "id", source = "modelId")
+    BuyerDTO toDTO(MongoBuyer buyerDTO);
+
+    @Mapping(target = "modelId", source = "id")
+    @Mapping(target = "id", ignore = true)
+    MongoBuyer toMongoEntity(BuyerDTO buyerDTO);
+
+    @Mapping(target = "id", ignore = true)
+    MongoBuyer entityToMongo(Buyer buyer);
 }
